007079b5319343ed93af3009ad39da67276af304
мария <maripav02@gmail.com>
2021-10-02 17:15:47 +0400

    1.7

--- src/main/java/ru/ssau/tk/way1/practice/Points.java
+++ src/main/java/ru/ssau/tk/way1/practice/Points.java
@@ -6,38 +6,45 @@ public class Points  {
         double n= u.x + v.x;
         double m =u.y + v.y;
         double l= u.z+ v.z;
-        Point t = new Point(n,m,l);
         return new Point(n, m, l);
     }
     static Point subtract(Point u,  Point v){
         double n= v.x - u.x;
         double m =v.y - u.y;
         double l= v.z - u.z;
-        Point t = new Point(n,m,l);
         return new Point(n, m, l);
     }
     static Point multiply(Point u, Point v){
         double n= u.x * v.x;
         double m =u.y * v.y;
         double l= u.z * v.z;
-        Point t = new Point(n,m,l);
         return new Point(n, m, l);
     }
     static Point divide (Point u, Point v){
             double n = u.x / v.x;
             double m = u.y / v.y;
             double l = u.z / v.z;
-            Point t = new Point(n, m, l);
             return new Point(n, m, l);
     }
     static Point enlarge (Point u, double v){
         double n = u.x * v;
         double m = u.y * v;
         double l = u.z * v;
-        Point t = new Point(n, m, l);
         return new Point(n, m, l);
     }
     static double Length (Point point){
         return point.Length();
+    }
+    static Point opposite(Point point){
+        double n = -1*point.x;
+        double m = -1*point.y;
+        double l = -1*point.z ;
+        return new  Point(n, m, l);
+    }
+    static Point inverse(Point point){
+        double n = 1/point.x;
+        double m = 1/point.y;
+        double l = 1/point.z ;
+        return new  Point(n, m, l);
     }
 }
