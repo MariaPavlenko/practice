a2a168c908ba1a6aaab96a84976cf47a39ef9fe6
мария <maripav02@gmail.com>
2021-10-02 18:02:01 +0400

    1.10

---
+++ src/test/java/ru/ssau/tk/way1/practice/PersonTest.java
@@ -0,0 +1,31 @@
+package ru.ssau.tk.way1.practice;
+
+import static org.testng.Assert.*;
+
+public class PersonTest {
+
+    @org.testng.annotations.Test
+    public void testGetFirstName() {
+    }
+
+    @org.testng.annotations.Test
+    public void testGetLastName() {
+    }
+
+    @org.testng.annotations.Test
+    public void testGetPassportId() {
+    }
+
+    @org.testng.annotations.Test
+    public void testSetFirstName() {
+
+    }
+
+    @org.testng.annotations.Test
+    public void testSetLastName() {
+    }
+
+    @org.testng.annotations.Test
+    public void testSetPassportId() {
+    }
+}
