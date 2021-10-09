915d3d4159d55e563ca87495cae4fe4c463d5caf
мария <maripav02@gmail.com>
2021-10-09 11:55:57 +0400

    1.10

--- src/test/java/ru/ssau/tk/way1/practice/PersonTest.java
+++ src/test/java/ru/ssau/tk/way1/practice/PersonTest.java
@@ -3,29 +3,22 @@ package ru.ssau.tk.way1.practice;
 import static org.testng.Assert.*;
 
 public class PersonTest {
-
     @org.testng.annotations.Test
-    public void testGetFirstName() {
+    public  void test(){
+        Person bob = new Person();
+        Person oleg = new Person();
+        bob.setPassportId(575);
+        bob.setFirstName("Bob");
+        bob.setLastName("Kot");
+        assertEquals(bob.getPassportId(),575);
+        assertEquals(bob.getFirstName(), "Bob");
+        assertEquals(bob.getLastName(), "Kot");
+        oleg.setPassportId(626);
+        oleg.setFirstName("Oleg");
+        oleg.setLastName("Losik");
+        assertEquals(oleg.getPassportId(),626);
+        assertEquals(oleg.getFirstName(), "Oleg");
+        assertEquals(oleg.getLastName(), "Losik");
     }
 
-    @org.testng.annotations.Test
-    public void testGetLastName() {
-    }
-
-    @org.testng.annotations.Test
-    public void testGetPassportId() {
-    }
-
-    @org.testng.annotations.Test
-    public void testSetFirstName() {
-
-    }
-
-    @org.testng.annotations.Test
-    public void testSetLastName() {
-    }
-
-    @org.testng.annotations.Test
-    public void testSetPassportId() {
-    }
 }
---
+++ src/test/java/ru/ssau/tk/way1/practice/PersonTestone.java
@@ -0,0 +1,29 @@
+package ru.ssau.tk.way1.practice;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class PersonTestone {
+
+    @Test
+    public void testGetFirstName() {
+        Person bob = new Person();
+        bob.setFirstName("Bob");
+        assertEquals(bob.getFirstName(),"Bob");
+    }
+
+    @Test
+    public void testGetLastName() {
+        Person bob= new Person();
+        bob.setLastName("Kot");
+        assertEquals(bob.getLastName(),"Kot");
+    }
+
+    @Test
+    public void testGetPassportId() {
+        Person bob= new Person();
+        bob.setPassportId(676);
+        assertEquals(bob.getPassportId(), 676);
+    }
+}
