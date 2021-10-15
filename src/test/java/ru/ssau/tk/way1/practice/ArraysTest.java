f55608c792deaa48b7bfcf021069d3976124cb16
мария <maripav02@gmail.com>
2021-10-15 20:40:57 +0400

    2.1

---
+++ src/test/java/ru/ssau/tk/way1/practice/ArraysTest.java
@@ -0,0 +1,14 @@
+package ru.ssau.tk.way1.practice;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class ArraysTest {
+
+    @Test
+    public void testArr() {
+
+
+    }
+}
