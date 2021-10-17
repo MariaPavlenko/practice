a6e3f4d48b0c88fd7bdd5e9a98a3d04e711cb08b
мария <maripav02@gmail.com>
2021-10-17 21:21:13 +0400

    2.1

--- src/test/java/ru/ssau/tk/way1/practice/ArraysTest.java
+++ src/test/java/ru/ssau/tk/way1/practice/ArraysTest.java
@@ -1,14 +1,16 @@
 package ru.ssau.tk.way1.practice;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.*;
 
 public class ArraysTest {
+    Arrays arr1 = new Arrays();
+    int[] first = arr1.arr(5);
 
     @Test
     public void testArr() {
-
-
+        Assert.assertEquals(first.length, 5);
     }
 }
