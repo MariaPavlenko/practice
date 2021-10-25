bada0871d46720050948ab9a63e9105cd377e677
мария <maripav02@gmail.com>
2021-10-25 15:39:33 +0400

    3.1

---
+++ src/main/java/ru/ssau/tk/way1/practice/Strings.java
@@ -0,0 +1,9 @@
+package ru.ssau.tk.way1.practice;
+
+public class Strings {
+    public void stringByChara(String s) {
+        for (int i = 0; i < s.length(); i++) {
+            System.out.println(s.charAt(i));
+        }
+    }
+}
